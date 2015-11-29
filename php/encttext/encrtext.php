0f7ff8519939d0bdb4cad7570c37f78da944bdd6
unknown <xyzxyzxyzxyzxz@gmail.com>
2015-11-29 09:43:23 +0800

    1a

---
+++ php/encttext/encrtext.php
@@ -0,0 +1,26 @@
+/*use the ascii value of a char to add integer to encryption */
+
+/*end in AD 2015_11_29_09:41:02_GMT+8:00*/
+
+
+<?php
+
+$a="mqyi";
+$b=="zqx";
+$i=0;
+
+for($i=0;$i<4;$i++);
+{
+  $a[$i]=chr(ord($a[$i])-$i-1);
+}
+for($i=0;i<3;$i++)
+{
+  $b[$i]=chr(ord($b[$i])-$i-1);
+}
+
+echo "$a $b";
+
+?>
+
+
+/*S_Y*//*X Y Z*/
